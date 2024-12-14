3bf8f86e96e04ae9d38b967bb18d4b20fe7d2794
zakkhoyt <zakkhoyt@hatchbaby.com>
2024-12-14 07:24:59 -0700

    Initial Commit

---
+++ SwiftMemoryAndPointers/SwiftMemoryAndPointersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMemoryAndPointersApp.swift
+//  SwiftMemoryAndPointers
+//
+//  Created by Zakk Hoyt on 2024-12-14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMemoryAndPointersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftMemoryAndPointersTests/SwiftMemoryAndPointersTests.swift
@@ -0,0 +1,16 @@
+//
+//  SwiftMemoryAndPointersTests.swift
+//  SwiftMemoryAndPointersTests
+//
+//  Created by Zakk Hoyt on 2024-12-14.
+//
+
+import Testing
+
+struct SwiftMemoryAndPointersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
